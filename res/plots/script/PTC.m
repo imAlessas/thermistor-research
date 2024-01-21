f15359d774f30adddd264e1a83dfb842c2d5c29f
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-01-21 16:47:56 +0200

    added other plots

--- res/plots/script/PTC.m
+++ res/plots/script/PTC.m
@@ -1,4 +1,4 @@
-clc, clf, clear, clearvars
+clc, clear, clearvars, close all
 
 % Define temperature range from 260 K to 420 K with intervals of 0.5 K
 T = 260 : 1/2 : 420;
@@ -16,14 +16,46 @@ R = R0 * exp(beta * (1 ./ T - 1/T0));
 % Calculate resistance ratio normalized to the resistance at 25°C
 R_ratio = R ./ R((25 + 273 - 260) * 2);
 
+% Set position vector for figure windows
+pos_vector = [250, 100, 1000, 650];
+
 % Plot 1: Resistance vs. Temperature (semilogarithmic scale)
-figure(1)
+f = figure(1);
+f.Position = pos_vector;
 semilogy(T, R), grid on;
 xlim([270 420]);
-xlabel("Temperature [K]"), ylabel("Resistance [Ω]")
+xlabel("Temperature [K]"), ylabel("Resistance [Ω]"), title("Resistance - Temperature")
 
 % Plot 2: Resistance Change (R/R25) vs. Temperature
-figure(2)
+f = figure(2);
+f.Position = pos_vector;
 plot(T, R_ratio), grid on;
 xlim([300 420]), ylim([-500 1e4])
-xlabel("Temperature [K]"), ylabel("Resistance Change (R/R25)")
+xlabel("Temperature [K]"), ylabel("Resistance Change (R/R25)"), title("Resistance change ratio - Temperature")
+
+% Define dissipation constant and reference temperature
+dissipation_constant = 4.5e-3;
+T0 = 298.15;
+
+% Calculate power, voltage, and current
+P = dissipation_constant * (T - T0);
+V = sqrt(R .* P);
+I = sqrt(P ./ R);
+
+% Plot 3: Current vs. Temperature (log-log scale)
+f = figure(3);
+f.Position = pos_vector;
+loglog(T, I), grid on;
+xlabel("Temperature [K]"), ylabel("Current [A]"), title("Current - Temperature")
+
+% Plot 4: Voltage vs. Temperature (log-log scale)
+f = figure(4);
+f.Position = pos_vector;
+loglog(T, V), grid on;
+xlabel("Temperature [K]"), ylabel("Voltage [V]"), title("Voltage - Temperature")
+
+% Plot 5: Current vs. Voltage (log-log scale)
+f = figure(5);
+f.Position = pos_vector;
+loglog(V, I), grid on;
+xlabel("Voltage [V]"), ylabel("Current [A]"), title("Current - Voltage")
